3902a08631c0f70845e6a798dceba6003984077b
Bishal Shrestha <bishalsthaa@bishals-mbp.attlocal.net>
2021-06-15 14:31:01 -0500

    changes on the dashboard.page html, also added status, main and navbar html. And finally added logo

--- ALPR_System/ALPR_System/settings.py
+++ ALPR_System/ALPR_System/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'phone_field',
+
+    #'accounts'
+    #'phone_field',
+    
     'accounts.apps.AccountsConfig',
 
 ]
